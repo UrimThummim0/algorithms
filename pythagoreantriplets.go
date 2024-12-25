2a38c56df808caa23a708468ceea1a6d7b58de5d
UrimThummim0 <rasolofondraiben02@gmail.com>
2024-12-25 18:18:24 +0300

    small update

--- pythagoreantriplets.go
+++ pythagoreantriplets.go
@@ -4,13 +4,13 @@ import "fmt"
 
 // pythagoreanTripletsBruteForce finds the Pythagorean triplet where the sum of its
 // numbers is 1000, using a brute force approach.
-func pythagoreanTripletsBruteForce() {
+func pythagoreanTripletsBruteForce(sum int) {
 	// Iterate through all possible values of a, b, and c, and check if they form
 	// a Pythagorean triplet whose sum is 1000.
-	for a := 1; a <= 1000; a++ {
-		for b := a + 1; b < 1000-a; b++ {
-			for c := b + 1; c < 1000-b; c++ {
-				if a*a+b*b == c*c && a+b+c == 1000 {
+	for a := 1; a <= sum; a++ {
+		for b := a + 1; b < sum-a; b++ {
+			for c := b + 1; c < sum-b; c++ {
+				if a*a+b*b == c*c && a+b+c == sum {
 					fmt.Println("a =", a, "b =", b, "c =", c)
 					return
 				}
@@ -21,5 +21,5 @@ func pythagoreanTripletsBruteForce() {
 
 func main() {
 	fmt.Println("Pythagorean Triplets")
-	pythagoreanTripletsBruteForce()
+	pythagoreanTripletsBruteForce(10000)
 }
