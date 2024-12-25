64f4e4f8542a54717dd1105eedac122b295d4584
UrimThummim0 <rasolofondraiben02@gmail.com>
2024-12-25 17:51:43 +0300

    PythagoreanTriplet Brute Force Algorithm.

--- pythagoreantriplets.go
+++ pythagoreantriplets.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func pythagoreanTriplets() {
+func pythagoreanTripletsBruteForce() {
 	for a := 1; a <= 1000; a++ {
 		for b := a + 1; b < 1000; b++ {
 			for c := b + 1; c < 1000; c++ {
